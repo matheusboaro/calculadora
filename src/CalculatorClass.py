f07d958655b2728bf3be0d4cf75c2916b3dd97e3
Jose Boaro <boaro.m@hotmail.com>
2024-10-15 18:52:46 -0300

    [DEV] Boaro - Classe inicial de calculadora

---
+++ src/CalculatorClass.py
@@ -0,0 +1,17 @@
+
+class Calculator:
+    def __init__(self):
+        pass
+
+    def adicao(self):
+        pass
+
+    def subtracao(self):
+        pass
+
+    def multiplicacao(self):
+        pass
+    
+    def divisao(self):
+        pass
+    
