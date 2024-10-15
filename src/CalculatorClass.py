be76505cd1c6a6ed7e7155b68b0dd0465b96ca44
Jose Boaro <boaro.m@hotmail.com>
2024-10-15 18:54:40 -0300

    [DEV] Boaro - Feature adicao

--- src/CalculatorClass.py
+++ src/CalculatorClass.py
@@ -3,15 +3,16 @@ class Calculator:
     def __init__(self):
         pass
 
-    def adicao(self):
-        pass
+    def adicao(self,a,b):
+
+        return a+b
 
     def subtracao(self):
         pass
 
     def multiplicacao(self):
         pass
-    
+
     def divisao(self):
         pass
     
